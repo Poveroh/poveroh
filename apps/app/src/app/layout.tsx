06082bbca9383d736cdc2b6f9921b7c8a68089ad
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-02-04 23:18:28 +0100

    Init app

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -1,20 +1,20 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next'
 
-import "../styles/style.css";
+import '../styles/style.css'
 
 export const metadata: Metadata = {
-    title: "Poveroh",
-    description: "A unified platform to track your wealth",
-};
+    title: 'Poveroh',
+    description: 'A unified platform to track your wealth'
+}
 
 type RootLayoutProps = Readonly<{
-    children: React.ReactNode;
-}>;
+    children: React.ReactNode
+}>
 
 export default function RootLayout({ children }: RootLayoutProps) {
     return (
-        <html lang="en">
+        <html lang='en'>
             <body>{children}</body>
         </html>
-    );
+    )
 }
