83a766d1b7ef421f09229bb34fe961ee9eef26a6
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-02-08 18:30:37 +0100

    Init app layout

--- apps/app/app/layout.tsx
+++ apps/app/app/layout.tsx
@@ -1,17 +1,5 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-
 import '@poveroh/ui/globals.css'
-import { Providers } from '@/components/providers'
-
-const fontSans = Geist({
-    subsets: ['latin'],
-    variable: '--font-sans'
-})
-
-const fontMono = Geist_Mono({
-    subsets: ['latin'],
-    variable: '--font-mono'
-})
+import { ThemeProviders } from '@/components/themeProvider'
 
 export default function RootLayout({
     children
@@ -20,8 +8,8 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en' suppressHydrationWarning>
-            <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}>
-                <Providers>{children}</Providers>
+            <body className='antialiased'>
+                <ThemeProviders>{children}</ThemeProviders>
             </body>
         </html>
     )
