2ebe8eec0b10236ad3a4609fcdbe145d8bc82c3d
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-02-08 16:55:28 +0100

    Add logo component

---
+++ packages/ui/src/components/logo.tsx
@@ -0,0 +1,17 @@
+export type LogoProps = {
+    color: 'black' | 'white'
+    mode: 'horizontal' | 'vertical' | 'symbol' | 'written'
+    width: number
+    height: number
+}
+
+export function Logo(props: LogoProps) {
+    return (
+        <img
+            src={`/logo/${props.color}/${props.mode}.svg`}
+            alt='logo'
+            width={props.width}
+            height={props.height}
+        />
+    )
+}
