5cdd08a45d186e122a4d1eeffb18c53d2867fc87
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-02-11 00:10:21 +0100

    Add UI components first version

--- packages/ui/src/components/form.tsx
+++ packages/ui/src/components/form.tsx
@@ -90,7 +90,7 @@ const FormLabel = React.forwardRef<
     return (
         <Label
             ref={ref}
-            className={cn(error && 'text-destructive', className)}
+            className={cn(error && 'text-danger', className)}
             htmlFor={formItemId}
             {...props}
         />
@@ -128,7 +128,7 @@ const FormDescription = React.forwardRef<
         <p
             ref={ref}
             id={formDescriptionId}
-            className={cn('text-sm text-muted-foreground', className)}
+            className={cn('text-base text-muted-foreground', className)}
             {...props}
         />
     )
@@ -150,7 +150,7 @@ const FormMessage = React.forwardRef<
         <p
             ref={ref}
             id={formMessageId}
-            className={cn('text-sm font-medium text-destructive', className)}
+            className={cn('text-base font-medium text-danger', className)}
             {...props}
         >
             {body}
