5fd030729d30cf8d41470cb1e97e2d87c5237fe3
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-02-22 14:05:01 +0100

    Add sign-in function

--- apps/app/lib/auth/auth.ts
+++ apps/app/lib/auth/auth.ts
@@ -1,3 +1,11 @@
-import { IUserLogin } from "@poveroh/types";
+import { type IUserLogin, ServerRequest } from '@poveroh/types'
+import { send } from '@/lib/server'
+import router from 'next/router'
 
-function signIn(user: IUserLogin ) {}
+function signIn(user: IUserLogin) {
+    send<boolean>(ServerRequest.POST, '/auth/login', user, 'login').then(() => {
+        router.push('/dashboard')
+    })
+}
+
+export { signIn }
