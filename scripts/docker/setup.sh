d3031570dbebe04a76935cddfc2a6690a01433bd
Davide Tarditi <36693369+DavideTarditi@users.noreply.github.com>
2025-11-16 18:55:32 +0100

    fix(setup): improve error handling and user feedback in setup script

--- scripts/docker/setup.sh
+++ scripts/docker/setup.sh
@@ -11,18 +11,21 @@ fetch_file_from_github() {
     local file_path="$1"
     local destination="$2"
 
+    echo "Fetching $file_path from GitHub..."
     # Ensure the destination directory exists
     local destination_dir
     destination_dir=$(dirname "$destination")
     if [[ ! -d "$destination_dir" ]]; then
+        echo "Creating directory $destination_dir..."
         mkdir -p "$destination_dir"
     fi
 
     curl -sSL "$GITHUB_REPO_URL/$file_path" -o "$destination"
     if [[ $? -ne 0 ]]; then
-        echo "Error downloading $file_path from GitHub."
+        echo "Error: Failed to download $file_path from GitHub."
         exit 1
     fi
+    echo "$file_path downloaded successfully to $destination."
 }
 
 # Function to detect architecture and set image configuration
@@ -42,7 +45,7 @@ detect_architecture_and_configure() {
             export IMAGE_ARCH="arm64"
             ;;
         *)
-            echo "Unsupported architecture: $arch"
+            echo "Error: Unsupported architecture: $arch"
             exit 1
             ;;
     esac
@@ -52,29 +55,38 @@ detect_architecture_and_configure() {
 
 # Function to download necessary files
 download_files() {
-    echo "Downloading necessary files..."
+    echo "Starting file download process..."
 
     if [[ ! -f "$COMPOSE_FILE" ]]; then
         echo "Downloading docker-compose.prod.yml file..."
         fetch_file_from_github "docker/docker-compose.prod.yml" "$COMPOSE_FILE"
+    else
+        echo "docker-compose.prod.yml already exists. Skipping download."
     fi
 
     if [[ ! -f "$ENV_FILE" ]]; then
         echo "Downloading poveroh.sample file..."
         fetch_file_from_github ".env.prod.example" "$ENV_FILE"
+    else
+        echo "$ENV_FILE already exists. Skipping download."
     fi
 
-    echo "Files downloaded successfully."
+    echo "All necessary files are downloaded."
 }
 
 # Function to download Docker images
 download_images() {
-    echo "Downloading Docker images for $IMAGE_ARCH..."
+    echo "Starting Docker image download process for $IMAGE_ARCH..."
     docker compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" pull
-    echo "Docker images downloaded successfully."
+    if [[ $? -eq 0 ]]; then
+        echo "Docker images downloaded successfully."
+    else
+        echo "Error: Failed to download Docker images."
+        exit 1
+    fi
 }
 
-#Function to configure environment file
+# Function to configure environment file
 configure_env_file() {
     echo "Configuring environment file..."
 
@@ -83,6 +95,7 @@ configure_env_file() {
         local current_jwt_key
         current_jwt_key=$(grep '^JWT_KEY=' "$ENV_FILE" | cut -d '=' -f 2)
         if [[ -z "$current_jwt_key" ]]; then
+            echo "JWT_KEY is empty. Generating a new key..."
             local jwt_key
             jwt_key=$(openssl rand -hex 32)
             sed -i '' "s|^JWT_KEY=.*|JWT_KEY=$jwt_key|" "$ENV_FILE"
@@ -91,6 +104,7 @@ configure_env_file() {
             echo "JWT_KEY already exists and is set in $ENV_FILE."
         fi
     else
+        echo "JWT_KEY not found. Adding a new key..."
         local jwt_key
         jwt_key=$(openssl rand -hex 32)
         echo "JWT_KEY=$jwt_key" >>"$ENV_FILE"
@@ -99,17 +113,20 @@ configure_env_file() {
 
     # Set CDN_LOCAL_DATA_PATH to the root of the folder
     if ! grep -q '^CDN_LOCAL_DATA_PATH=' "$ENV_FILE"; then
+        echo "Adding CDN_LOCAL_DATA_PATH to $ENV_FILE..."
         echo "CDN_LOCAL_DATA_PATH=$PROJECT_ROOT/cdn-data" >>"$ENV_FILE"
         echo "Set CDN_LOCAL_DATA_PATH to $PROJECT_ROOT/cdn-data in $ENV_FILE."
     else
+        echo "Updating CDN_LOCAL_DATA_PATH in $ENV_FILE..."
         sed -i '' "s|^CDN_LOCAL_DATA_PATH=.*|CDN_LOCAL_DATA_PATH=$PROJECT_ROOT/cdn-data|" "$ENV_FILE"
         echo "Updated CDN_LOCAL_DATA_PATH to $PROJECT_ROOT/cdn-data in $ENV_FILE."
     fi
 
     # Create the cdn-data folder if it doesn't exist
     if [[ ! -d "$PROJECT_ROOT/cdn-data" ]]; then
+        echo "Creating folder $PROJECT_ROOT/cdn-data..."
         mkdir -p "$PROJECT_ROOT/cdn-data"
-        echo "Created folder $PROJECT_ROOT/cdn-data"
+        echo "Folder $PROJECT_ROOT/cdn-data created."
     else
         echo "Folder $PROJECT_ROOT/cdn-data already exists."
     fi
@@ -118,14 +135,18 @@ configure_env_file() {
 
     # Add IMAGE_ARCH to the environment file
     if ! grep -q '^IMAGE_ARCH=' "$ENV_FILE"; then
+        echo "Adding IMAGE_ARCH to $ENV_FILE..."
         echo "IMAGE_ARCH=$IMAGE_ARCH" >> "$ENV_FILE"
         echo "Added IMAGE_ARCH=$IMAGE_ARCH to $ENV_FILE."
     else
+        echo "Updating IMAGE_ARCH in $ENV_FILE..."
         sed -i '' "s|^IMAGE_ARCH=.*|IMAGE_ARCH=$IMAGE_ARCH|" "$ENV_FILE"
         echo "Updated IMAGE_ARCH to $IMAGE_ARCH in $ENV_FILE."
     fi
 
+    echo "Copying environment file to .env..."
     copy_env_file
+    echo "Environment file copied successfully."
 }
 
 # Function to copy environment file
@@ -136,14 +157,18 @@ copy_env_file() {
 # Function to start or update images
 start_images() {
     echo "Starting Docker containers..."
-    docker network ls | grep -w poveroh_network || docker network create poveroh_network
+    docker network ls | grep -w poveroh_network || {
+        echo "Creating Docker network poveroh_network..."
+        docker network create poveroh_network
+        echo "Docker network poveroh_network created."
+    }
 
     echo "Starting or updating images..."
     local database_host
     database_host=$(grep -E '^DATABASE_HOST=' "$ENV_FILE" | cut -d '=' -f 2)
 
     if [[ -z "$database_host" ]]; then
-        echo "DATABASE_HOST is not set in $ENV_FILE."
+        echo "Error: DATABASE_HOST is not set in $ENV_FILE."
         exit 1
     fi
 
@@ -153,12 +178,18 @@ start_images() {
     fi
 
     docker compose -f "$COMPOSE_FILE" --env-file "$ENV_FILE" up -d
-
-    echo "Images started or updated successfully."
+    if [[ $? -eq 0 ]]; then
+        echo "✅ Docker containers started successfully."
+        echo "🚀 App is running at http://app.poveroh.local"
+    else
+        echo "Error: Failed to start Docker containers."
+        exit 1
+    fi
 }
 
 # Ensure proper function call syntax
 stop_images() {
+    echo "Preparing to stop Docker images..."
     local stop_choice="$1"
 
     if [[ -z "$stop_choice" ]]; then
@@ -173,19 +204,19 @@ stop_images() {
         1)
             echo "Stopping all images..."
             docker compose -f "$COMPOSE_FILE" down
+            echo "All images stopped successfully."
             ;;
         2)
             echo "Enter the names of the services to stop (separated by space):"
             read -r services_to_stop
             echo "Stopping services: $services_to_stop..."
             docker compose -f "$COMPOSE_FILE" stop $services_to_stop
+            echo "Selected services stopped successfully."
             ;;
         *)
-            echo "Invalid option."
+            echo "Invalid option. Please try again."
             ;;
     esac
-
-    echo "Images stopped successfully."
 }
 
 # Function to inject platform configuration into docker-compose.prod.yml
